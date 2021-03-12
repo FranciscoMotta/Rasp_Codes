3efede2c788c05368fc6c2d59cc3b30313ff1a27
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2021-03-12 17:43:45 -0500

    Agregamos code push up

--- pulsador_pull_up.py
+++ pulsador_pull_up.py
@@ -1,8 +1,35 @@
 import RPi.GPIO as GPIO
 import time
 
+# Advertencias
+
+GPIO.setwarnings(False)
+
+# Enumeracion mediante GPIO
+
+GPIO.setmode(GPIO.BCM) 
+
+# Configuracion de PINES 
+
+pushBotton = 21
+led = 2 
+
+# Configuramos el pulsador como entrada pull up
+
+GPIO.setup(pushBotton, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# Led
+
+GPIO.setup(led, GPIO.OUT)
+
 try:
 
+	if GPIO.input(pushBotton) == False:
+		print("pulsador presionado")
+		GPIO.output(led, GPIO.HIGH)
+		time.sleep(0.1)
+	else:
+		GPIO.output(led, LOW)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
